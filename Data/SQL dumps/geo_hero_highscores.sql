f5f164ed82c41bbd3d3b4f5c618f2a230f829fef
sleepwalkershorty <sleepwalkershorty@web.de>
2016-09-29 15:56:51 +0200

    SQL Tests

---
+++ Data/SQL dumps/geo_hero_highscores.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `geo_hero` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `geo_hero`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: geo_hero
+-- ------------------------------------------------------
+-- Server version	5.7.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `highscores`
+--
+
+DROP TABLE IF EXISTS `highscores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `highscores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `users_id` int(10) unsigned NOT NULL,
+  `points` int(11) DEFAULT NULL,
+  `time` int(11) DEFAULT NULL,
+  `erros` int(11) DEFAULT NULL,
+  `timestamp` int(11) DEFAULT NULL,
+  `game_type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`users_id`),
+  KEY `fk_highscores_users_idx` (`users_id`),
+  CONSTRAINT `fk_highscores_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `highscores`
+--
+
+LOCK TABLES `highscores` WRITE;
+/*!40000 ALTER TABLE `highscores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `highscores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-29 15:54:41
---
+++ Data/SQL dumps/test.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `country` (
+  `Code` char(3) NOT NULL DEFAULT '',
+  `Name` char(52) NOT NULL DEFAULT '',
+  `Continent` enum('Asia','Europe','North America','Africa','Oceania','Antarctica','South America') NOT NULL DEFAULT 'Asia',
+  `Region` char(26) NOT NULL DEFAULT '',
+  `SurfaceArea` float(10,2) NOT NULL DEFAULT '0.00',
+  `IndepYear` smallint(6) DEFAULT NULL,
+  `Population` int(11) NOT NULL DEFAULT '0',
+  `LifeExpectancy` float(3,1) DEFAULT NULL,
+  `GNP` float(10,2) DEFAULT NULL,
+  `GNPOld` float(10,2) DEFAULT NULL,
+  `LocalName` char(45) NOT NULL DEFAULT '',
+  `GovernmentForm` char(45) NOT NULL DEFAULT '',
+  `HeadOfState` char(60) DEFAULT NULL,
+  `Capital` int(11) DEFAULT NULL,
+  `Code2` char(2) NOT NULL DEFAULT '',
+  PRIMARY KEY (`Code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
